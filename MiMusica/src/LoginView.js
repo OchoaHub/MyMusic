0eeeaa59dcc713bc3aa4d7fb6c00132af44102de
5j <5j@iMac-de-Facultad-2.local>
2019-11-06 08:28:44 -0600

    second commit

--- MiMusica/src/LoginView.js
+++ MiMusica/src/LoginView.js
@@ -3,7 +3,7 @@ import {
     Platform,
     StyleSheet,
     View,
-    tEXT,
+    Text,
     Image,
     TextInput,
     TouchableOpacity,
@@ -36,7 +36,7 @@ export default class LoginView extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Image source={require('./assets(ucol.png')} style={styles.logo} resizeMode="contain"/>
+                <Image source={require('./assets/ucol.png')} style={styles.logo} resizeMode="contain"/>
                 <TextInput
                     style={styles.TextInput}
                     onChangeText={(username) => this.setState({username})}
@@ -65,3 +65,47 @@ export default class LoginView extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: null,
+        height: null,
+        backgroundColor: 'white',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 200,
+        height: 150,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        marginTop: Platform.select({
+            ios: 90,
+           android: 50
+        }),
+        marginBottom: 25
+    },
+    TextInput: {
+        height: 40,
+        backgroundColor: '#f2f2f2',
+        width: 230,
+        color: '#000035',
+        marginBottom: 40,
+        borderRadius: 5,
+        padding: 5,
+    },
+    boton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 230,
+        height: 40,
+        backgroundColor: '#00ccd0',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#fff'
+    },
+    textoBoton: {
+        color: '#fff',
+        fontSize: 22,
+    },
+});
